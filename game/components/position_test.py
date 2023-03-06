d896b67ff3f7a7ce57c52fc53d2d2b9f95ba0057
Tomasz Nowakowski <toalak@gmail.com>
2023-03-06 19:33:57 +0100

    Updating tests for Position component by removing test_move_towards (that functionality is part of Being class now) and replacing it with test_return_px_to_cell.

--- game/components/position_test.py
+++ game/components/position_test.py
@@ -4,7 +4,7 @@
 from game.components.position import Position
 
 
-def test_move_towards():
-    tposition = Position(100, 100)
-    tposition.move_towards(20, 40)
-    assert tposition.x == 120 and tposition.y == 140
+def test_return_px_to_cell():
+    tposition = Position(100, 180)
+    cell_position = tposition.return_px_to_cell()
+    assert cell_position.x == 1 and cell_position.y == 2
