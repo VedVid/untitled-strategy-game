9fc9674a86b8c117b7e2020d307d220c1a5c6389
Tomasz Nowakowski <toalak@gmail.com>
2023-03-07 12:38:45 +0100

    wip, beings done

--- game/sprite_tracker.py
+++ game/sprite_tracker.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 
-# Using facade pattern.
+# At first, it was using facade pattern.
 # Source: https://github.com/faif/python-patterns/blob/master/patterns/structural/facade.py
-# TODO: Using facade pattern leads to major code duplication here. Should be replaced by a single class.
-# TODO: Find another place to use facade.
+# but it lead to major code duplication.
 
 
 import arcade
 
 from . import globals
+from .being import Player, Enemy
 from .components.position import Position
+from .map_object import MapObject
 from .pathfinding import Pathfinder
 from .states import State
+from .tile import Tile
 
 
 class SpriteTracker:
@@ -23,9 +25,78 @@ class SpriteTracker:
     """
 
     def __init__(self, beings, grid):
-        self._map_objects_selected = MapObjectSelected(grid)
-        self._beings_selected = BeingsSelected(beings)
-        self._tiles_selected = TilesSelected(grid)
+        self._beings = beings
+        self._grid = grid
+        self._pathfinder = Pathfinder(grid)
+        self._beings_sprites_selected = arcade.SpriteList()
+        self._map_objects_sprites_selected = arcade.SpriteList()
+        self._tiles_sprites_selected = arcade.SpriteList()
+        self.mouse_position = None
+        self.player = None
+
+    def _reset_sprite_list_tiles(self):
+        self._tiles_sprites_selected.clear()
+
+    def _add_to_spritelist(self, entity, targeted=False):
+        sprite = entity.sprite_selected.arcade_sprite
+        if targeted:
+            sprite = entity.sprite_targeted.arcade_sprite
+        if isinstance(entity, Player) or isinstance(entity, Enemy):
+            self._beings_sprites_selected.append(
+                sprite
+            )
+        elif isinstance(entity, MapObject):
+            self._map_objects_sprites_selected.append(
+                sprite
+            )
+        elif isinstance(entity, Tile):
+            self._tiles_sprites_selected.append(
+                sprite
+            )
+
+    def _find(self, what):
+        if what == "player_beings":
+            what = self._beings.player_beings
+        for effect in self.player.attack.effects:
+            for coords in effect.target_positions:
+                try:
+                    pos = Position(
+                        self.player.cell_position.x + coords[0],
+                        self.player.cell_position.y + coords[1],
+                    )
+                    for e in what:
+                        # Find "yellow" entity - ie tile that player can click on.
+                        if (
+                                e.cell_position.x == pos.x
+                                and e.cell_position.y == pos.y
+                        ):
+                            self._add_to_spritelist(e)
+                        # Find "red" entity - ie tile that will be attacked when player clicks on yellow tile.
+                        if pos.x == self.mouse_position.x and pos.y == self.mouse_position.y:
+                            for coords2 in effect.attack_pattern:
+                                pos2 = Position(
+                                    pos.x + coords2[0],
+                                    pos.y + coords2[1],
+                                )
+                                if (
+                                        e.cell_position.x == pos2.x
+                                        and e.cell_position.y == pos2.y
+                                ):
+                                    self._add_to_spritelist(e, True)
+                except AttributeError:
+                    pass  # No valid player_being found.
+                except ValueError as e:
+                    print(f"{e} in sprite_tracker.BeingsSelected._find_player_beings_selected")
+
+    def _find_beings(self):
+        for player_ in self._beings.player_beings:
+            if player_.active:
+                self._beings_sprites_selected.append(player_.sprite_active.arcade_sprite)
+        if globals.state == State.TARGET:
+            self._find("player_beings")
+
+    def _find_map_objects(self):
+
 
     def track(self, mouse_position, player=None):
         self._tiles_selected.find(mouse_position, player)
