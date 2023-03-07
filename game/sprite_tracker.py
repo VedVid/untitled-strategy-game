c51d817cab7db389b38cf529bedc9e1f9bcad600
Tomasz Nowakowski <toalak@gmail.com>
2023-03-07 13:01:52 +0100

    ...

--- game/sprite_tracker.py
+++ game/sprite_tracker.py
@@ -56,11 +56,13 @@ class SpriteTracker:
 
     def _find(self, what):
         if what == "player_beings":
-            what = self._beings.player_beings
+            l = self._beings.player_beings
         elif what == "enemy_beings":
-            what = self._beings.enemy_beings
+            l = self._beings.enemy_beings
         elif what == "map_objects":
-            what = self._grid.map_objects.objects
+            l = self._grid.map_objects.objects
+        elif what == "tiles":
+            l = self._grid.tiles
         for effect in self.player.attack.effects:
             for coords in effect.target_positions:
                 try:
@@ -68,7 +70,7 @@ class SpriteTracker:
                         self.player.cell_position.x + coords[0],
                         self.player.cell_position.y + coords[1],
                     )
-                    for e in what:
+                    for e in l:
                         # Find "yellow" entity - ie tile that player can click on.
                         if (
                                 e.cell_position.x == pos.x
@@ -90,7 +92,7 @@ class SpriteTracker:
                 except AttributeError:
                     pass  # No valid player_being found.
                 except ValueError as e:
-                    print(f"{e} in sprite_tracker.BeingsSelected._find_player_beings_selected")
+                    print(f"{e} in sprite_tracker._find({what})")
 
     def _find_player_beings(self):
         for player in self._beings.player_beings:
@@ -106,6 +108,19 @@ class SpriteTracker:
     def _find_map_objects(self):
         if globals.state == State.TARGET:
             self._find("map_objects")
+
+    def _find_tiles(self):
+        if globals.state == State.MOVE:
+            try:
+                for coords in self._pathfinder.last_path:
+                    tile = self._grid.find_tile_by_position(
+                        Position(coords[0], coords[1])
+                    )
+                    self._tiles_sprites_selected.append(tile.sprite_path.arcade_sprite)
+            except TypeError:  # Empty last_path
+                pass
+        elif globals.state == State.TARGET:
+            self._find("tiles")
 
     def track(self, mouse_position, player=None):
         self._tiles_selected.find(mouse_position, player)
@@ -140,42 +155,6 @@ class MapObjectSelected:
     def _reset_sprite_list(self):
         self.sprites_selected.clear()
 
-    def _find_map_objects_selected(self, mouse_position, player=None):
-        if globals.state == State.TARGET:
-            for effect in player.attack.effects:
-                for coords in effect.target_positions:
-                    try:
-                        pos = Position(
-                            player.cell_position.x + coords[0],
-                            player.cell_position.y + coords[1],
-                        )
-                        for map_object in self.owner.map_objects.objects:
-                            # Find "yellow" object - ie tile that player can click on.
-                            if (
-                                map_object.cell_position.x == pos.x
-                                and map_object.cell_position.y == pos.y
-                            ):
-                                self.sprites_selected.append(
-                                    map_object.sprite_selected.arcade_sprite
-                                )
-                            # Find "red" object - ie tile that will be attacked when player clicks on yellow tile.
-                            if pos.x == mouse_position.x and pos.y == mouse_position.y:
-                                for coords2 in effect.attack_pattern:
-                                    pos2 = Position(
-                                        pos.x + coords2[0],
-                                        pos.y + coords2[1],
-                                    )
-                                    if (
-                                            map_object.cell_position.x == pos2.x
-                                            and map_object.cell_position.y == pos2.y
-                                    ):
-                                        self.sprites_selected.append(
-                                            map_object.sprite_targeted.arcade_sprite
-                                        )
-                    except AttributeError:
-                        pass  # No valid MapObject found.
-                    except ValueError as e:
-                        print(f"{e} in sprite_tracker.MapObjectSelected._find_map_objects_selected")
 
     def find(self, mouse_position, player=None):
         self._reset_sprite_list()
@@ -191,42 +170,6 @@ class TilesSelected:
         self.pathfinder = Pathfinder(grid)
         self.sprites_selected = arcade.SpriteList()
 
-    def _reset_sprite_list(self):
-        self.sprites_selected.clear()
-
-    def _find_tiles_selected(self, mouse_position, player=None):
-        if globals.state == State.MOVE:
-            try:
-                for coords in self.pathfinder.last_path:
-                    tile = self.grid.find_tile_by_position(
-                        Position(coords[0], coords[1])
-                    )
-                    self.sprites_selected.append(tile.sprite_path.arcade_sprite)
-            except TypeError:  # Empty last_path
-                pass
-        elif globals.state == State.TARGET:
-            for effect in player.attack.effects:
-                for coords in effect.target_positions:
-                    # TODO: Currently only first set of coords is used. Need to apply to all sets.
-                    try:
-                        tile_pos = Position(
-                            player.cell_position.x + coords[0],
-                            player.cell_position.y + coords[1],
-                        )
-                        tile = self.grid.find_tile_by_position(tile_pos)
-                        self.sprites_selected.append(tile.sprite_selected.arcade_sprite)
-                        if tile_pos.x == mouse_position.x and tile_pos.y == mouse_position.y:
-                            for coords2 in effect.attack_pattern:
-                                tile_pos2 = Position(
-                                    tile_pos.x + coords2[0],
-                                    tile_pos.y + coords2[1],
-                                )
-                                tile2 = self.grid.find_tile_by_position(tile_pos2)
-                                self.sprites_selected.append(tile2.sprite_targeted.arcade_sprite)
-                    except AttributeError:
-                        pass  # No valid Tile found.
-                    except ValueError as e:
-                        print(f"{e} in sprite_tracker.TilesSelected._find_tiles_selected")
 
     def find(self, mouse_position, player=None):
         self._reset_sprite_list()
