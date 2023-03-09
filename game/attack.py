d1beb8c67134c96aedbf2025ee7a6e52532fba50
Tomasz Nowakowski <toalak@gmail.com>
2023-03-09 16:31:02 +0100

    Docstring updated

--- game/attack.py
+++ game/attack.py
@@ -9,7 +9,8 @@ class Attack:
     Attacks are performed by Being instances. Every Attack may consist of multiple AttackEffect instances.
     E.g. if we would like to add an Attack that damages the targeted Being, and pushes all adjacent Being instances
     from the target, then we could achieve that by making an Attack with two effects: first, that decreases HP of
-    targeted Being, and second, that pushes away Being instances from the targeted Tile.
+    targeted Being, and second, that pushes away Being instances from the targeted Tile
+    Before performing an attack, it checks if cursor position is valid as effect target position.
     """
 
     def __init__(self, *args):
