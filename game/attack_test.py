35e785e4e05e9e4d09150bf3ed871bd4d3edfea1
Tomasz Nowakowski <toalak@gmail.com>
2023-03-28 22:09:18 +0200

    test_tiles_targeted_separate fixed

--- game/attack_test.py
+++ game/attack_test.py
@@ -31,19 +31,11 @@ def test_amount_of_attack_effects_separate():
     assert len(attack_2.effects) == 4
 
 
-#def test_tiles_targeted_separate():
-#    assert all(
-#        x in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-#        for x in attack_1.effects[0].target_positions
-#    )
-#    assert all(
-#        x in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-#        for x in attack_2.effects[0].target_positions
-#    )
-#    assert all(
-#        x in [(-1, 0), (1, 0), (0, -1), (0, 1)]
-#        for x in attack_2.effects[1].target_positions
-#    )
+def test_tiles_targeted_separate():
+    assert attack_1.effects[0].target_positions == [(-1, 0)]
+    assert attack_1.effects[1].target_positions == [(1, 0)]
+    assert attack_1.effects[2].target_positions == [(0, 1)]
+    assert attack_1.effects[3].target_positions == [(0, -1)]
 
 
 #def test_attack_pattern_separate():
