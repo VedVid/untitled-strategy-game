0d45df9dd867a28fe28bc920c2197ad3fa1f8613
Tomasz Nowakowski <toalak@gmail.com>
2023-03-28 23:46:57 +0200

    Fixed the root of the issue - it turned out that instead passing cursor_position that is always converted to the correct (cell / pixel) format, attack.perform was passing x, y values directly that could be either in px or cell format.

--- game/attack.py
+++ game/attack.py
@@ -28,7 +28,6 @@ class Attack:
             cursor_position = Position(x, y).return_px_to_cell()
             if not cursor:
                 cursor_position = Position(x, y)
-            print(cursor_position.x, cursor_position.y)
             valid_target_positions = []
             for target in effect.target_positions:
                 valid_target_position = (
@@ -36,13 +35,9 @@ class Attack:
                     self.owner.cell_position.y + target[1],
                 )
                 valid_target_positions.append(valid_target_position)
-            print(valid_target_positions)
             if (cursor_position.x, cursor_position.y) in valid_target_positions:
-                print("is valid")
-                effect.perform(beings, map_objects, x, y)
+                effect.perform(beings, map_objects, cursor_position.x, cursor_position.y)
                 performed = True
-            else:
-                print("is invalid")
         if performed:
             self.owner.attacked = True
             self.owner.moved = True
